e423f582ed76bb3163d5f8717121a04d8e3dd12c
AntoinePBAllard <allard.pb.allard@gmail.com>
2026-03-27 18:39:02 +0100

    rename - dvp

--- For CellMAP/averaging.m
+++
@@ -1,3 +0,0 @@
-function data_out = averaging(data_in)
-data_out = mean(data_in,3,'omitnan');
-end
--- For CellMAP/not used dofit.m
+++
@@ -1,4 +0,0 @@
-function fitted = dofit(myfit,deflection,force,valueProblems,valueCoefficients)
-
-    fitted = fit(deflection,force,myfit,'problem',valueProblems,'StartPoint',valueCoefficients,'Exclude',cond);
-end
